8533613385bb6951c896f0a033d0dd1101544ba1
bagus2705 <bagusprasetyo2705@gmail.com>
2021-12-02 19:47:52 +0700

    up

--- Board.java
+++ Board.java
@@ -3,56 +3,37 @@ import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.JFrame;
-import javax.swing.JMenu;
-import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 import javax.swing.JPanel;
 public class Board extends JFrame {
 
-public  int jumlahKotak = 9;
-public Grid [] kotak = new Grid[jumlahKotak];
+    private  int jumlahKotak = 9;
+    private Grid [] kotak = new Grid[jumlahKotak];
 
-public  Board(){
+    public  Board(){
 
-    super("Sudoku");
-    setSize(700,700);
-    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        super("Sudoku");
+        setSize(700,700);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
 
-    JPanel panel = new JPanel(new GridLayout(3,3));
-    for(int i=0; i<jumlahKotak; i++){
-        kotak[i] = new Grid();
-        panel.add(kotak[i]);
+        JPanel panel = new JPanel(new GridLayout(3,3));
+        for(int i=0; i<jumlahKotak; i++){
+            kotak[i] = new Grid();
+            panel.add(kotak[i]);
+        }
+        add(panel, BorderLayout.CENTER);
+        setVisible(true);
+        setLocationRelativeTo(null);
     }
 
-    JMenuBar menuBar = new JMenuBar();
-    setJMenuBar(menuBar);
-
-    JMenu menu = new JMenu("Menu");
-    menuBar.add(menu);
-
-    JMenuItem newLevel = new JMenuItem("Select New Level");
-    menu.add(newLevel);
-
-    JMenuItem reset = new JMenuItem("Reset");
-    menu.add(reset);
-
-    JMenuItem exit = new JMenuItem("Exit");
-    menu.add(exit);
-
-    add(panel, BorderLayout.CENTER);
-    setVisible(true);
-    setLocationRelativeTo(null);
-
-}
-
-public class exitaction implements ActionListener{
-    public void actionPerformed (ActionEvent e){
-        System.exit(0);
+    public class exitaction implements ActionListener{
+        public void actionPerformed (ActionEvent e){
+            System.exit(0);
+        }
     }
-}
 
-public static void main(String[] args) { 
-    Board board = new Board();
-}    
+    public static void main(String[] args) { 
+        Board board = new Board();
+    }    
 }
